3288e060b1ed4d2e31e68fd82450f03542d6fcac
rhinocrose938 <57975516+rhinocrose938@users.noreply.github.com>
2020-09-23 23:36:17 +0530

    added build

---
+++ build/precache-manifest.f293ab15bf39d879028fc52f4d6b09f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e7e8276b24b24666b4625e517932f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30c2e8970d603f7e47b3",
+    "url": "/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "c34794efc2e2c7b11bd3",
+    "url": "/static/css/main.8aa51518.chunk.css"
+  },
+  {
+    "revision": "30c2e8970d603f7e47b3",
+    "url": "/static/js/2.afcf11db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.afcf11db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c34794efc2e2c7b11bd3",
+    "url": "/static/js/main.dc2d57fb.chunk.js"
+  },
+  {
+    "revision": "c98dadfe2d27676abec4",
+    "url": "/static/js/runtime-main.d229ebd7.js"
+  }
+]);
